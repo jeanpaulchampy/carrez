babe6f1cb92d2cb42cb496fb13818c4a7fa77091
Jean-Paul <champy_jean-paul@live.fr>
2017-02-10 19:04:27 +0100

    remove console.log

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.post('/scrape', function(requete, response1) {
 
   var urlpost = requete.body.url;
-  console.log("urlpost=" + urlpost);
+  //console.log("urlpost=" + urlpost);
 
 
 
@@ -27,21 +27,21 @@ request(url, function (error, response, html) {
 
     var $=cheerio.load(html);
 
-        console.log("ok");
-        console.log($('.value').first().text());
+        //console.log("ok");
+        //console.log($('.value').first().text());
         var prix=$('.value').first().text();
 
       prix=prix.replace(/[^0-9]+/ig,"");
 
         var villeCP=$('.value').eq(1).text();
-        console.log($('.value').eq(2).text());
+        //console.log($('.value').eq(2).text());
         var typeDeBien=$('.value').eq(2).text();
 
         var surface=$('.value').eq(4).text();
         surface=surface.replace(/[^0-9]+/ig,"");
         surface=surface.slice(0,surface.length-1);
 
-        console.log(surface);
+        //console.log(surface);
 
 
  ////////////////////////////// meilleursagents
@@ -49,59 +49,59 @@ request(url, function (error, response, html) {
       ville=ville.replace(/\s/g,'');//remove space
       cp=villeCP.replace(/[^0-9]+/ig,"");
       urlMeilleursAgents="https://www.meilleursagents.com/prix-immobilier/"+ville+"-"+cp;
-      console.log(urlMeilleursAgents);
+      //console.log(urlMeilleursAgents);
       request(urlMeilleursAgents, function (error, response, html) {
         if (!error && response.statusCode == 200) {
-          console.log(html);
+          //console.log(html);
           var $=cheerio.load(html);
 
-          console.log("ok");
+          //console.log("ok");
 
           $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
             var data=$(this).text();
 
             prixMoyenAppartement=data;
             prixMoyenAppartement=prixMoyenAppartement.replace(/[^0-9]+/ig,"");
-            console.log(prixMoyenAppartement);
+            //console.log(prixMoyenAppartement);
           })
         $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
           var data=$(this).text();
 
           prixMoyenMaison=data;
           prixMoyenMaison=prixMoyenMaison.replace(/[^0-9]+/ig,"");
-          console.log(prixMoyenMaison);
+          //console.log(prixMoyenMaison);
         })
 
         if(typeDeBien==="Appartement")
         {
-            console.log(typeDeBien);
-            console.log(prix/surface);
+            //console.log(typeDeBien);
+            //console.log(prix/surface);
             if(prix/surface>prixMoyenAppartement)
             {
-              console.log("Mauvais deal");
+              //console.log("Mauvais deal");
 
               result="Mauvais Deal";
 
             }
             else
             {
-              console.log("Bon deal");
+            //  console.log("Bon deal");
               result="Bon Deal";
             }
 
         }
         if(typeDeBien==="Maison")
         {
-            console.log(typeDeBien);
-            console.log(prix/surface);
+            //console.log(typeDeBien);
+          //  console.log(prix/surface);
             if(prix/surface>prixMoyenMaison)
             {
-              console.log("Mauvais deal");
+            //  console.log("Mauvais deal");
               result="Mauvais Deal";
             }
             else
             {
-                console.log("Bon deal");
+            //    console.log("Bon deal");
                 result="Bon Deal";
             }
         }
